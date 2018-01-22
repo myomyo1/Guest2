48c2ef2f02845cc8a9f69454f473771fddf0a52b
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-22 20:27:52 +0900

    guest2

---
+++ WebContent/guestList.jsp
@@ -0,0 +1,60 @@
+﻿<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page import="com.javaex.vo.GuestVo"%>
+<%@ page import="java.util.List"%>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="gs" method="get">
+		<%
+			//<form action="add.jsp" method="post"> : add.jsp로 직접 부르지않고 controller에서
+		%>
+		<table border=1 width=500>
+			<tr>
+				<td>이름</td>
+				<td><input type="text" name="name"></td>
+				<td>비밀번호</td>
+				<td><input type="password" name="pass"> <input
+					type="hidden" name="a" value="add"> <!-- //value="add" : a의 초기값을 지정해주는 것 -->
+				</td>
+			</tr>
+			<tr>
+				<td colspan=4><textarea name="content" cols=60 rows=5></textarea></td>
+			</tr>
+			<tr>
+				<td colspan=4 align=right><input type="submit" VALUE=" 확인 "></td>
+			</tr>
+		</table>
+	</form>
+	<br />
+
+	<%
+		List<GuestVo> list = (List<GuestVo>) request.getAttribute("list"); //"list" 라는 이름을 붙여준 list<GuestVo>객체(=>gList라고했던애)가 옴. //넘어올때 object로 넘어왔으니 리스트로 강제 형변환해줌
+		for (GuestVo g : list) {
+	%>
+
+
+	<table width=510 border=1>
+		<tr>
+			<td><%=g.getNo()%></td>
+			<td><%=g.getName()%></td>
+			<td><a href="gs?a=guestDeleteform&no=<%=g.getNo()%>">삭제</a></td>
+		</tr>
+		<tr>
+			<td colspan=4><%=g.getContent()%></td>
+		</tr>
+	</table>
+	<br />
+	<%
+		}
+	%>
+
+
+</body>
+</html>
---
+++ src/com/javaex/controller/GusetServlet.java
@@ -0,0 +1,70 @@
+package com.javaex.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javaex.dao.GuestDao;
+import com.javaex.vo.GuestVo;
+
+@WebServlet("/gs")
+public class GusetServlet extends HttpServlet {
+	   
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//브라우저에서 받은 response, request를 controller에서 받아서 a로 넘긴 것
+	String actionName = request.getParameter("a");
+	if(actionName.equals("list")) {
+		System.out.println("list 진입");
+		GuestDao dao = new GuestDao();
+		List<GuestVo> glist = dao.showListAll(); //얘를 forword할때 request에 포함시켜줄 것. dao에 showListAll로 꺼내옴. list형태로 보여줘야하므로 list변수만들어서 저장
+		request.setAttribute("list", glist); //앞에 "list"는 나중에 getattribute로 받을 이름. 뒤에는 정보. setattribute가 request객체에다가 list객체(정보)를 붙여줌.
+		//controller에서 list로 포워드시키는 것.                    =대상
+		RequestDispatcher rd = request.getRequestDispatcher("guestList.jsp"); 
+		rd.forward(request, response); //url로 확인해보면 포워드 시킨 상태로 guestList.jsp화면이 보여짐.
+		
+	}else if(actionName.equals("add")) {
+		System.out.println("add 진입");
+		//name, pass, content 를 파라미터로 받아서 변수에 저장한 것.
+		String name = request.getParameter("name");
+		String password = request.getParameter("pass");
+		String content = request.getParameter("content");
+		//위의 값들을 객체로 만들어서 다오에게 넘겨줘야함
+		GuestVo gvo = new GuestVo(name, password, content) ;
+		GuestDao gdao = new GuestDao();
+		gdao.insert(gvo);
+		
+		response.sendRedirect("gs?a=list"); //리다이렉트 여기서 "list.jsp" X , 컨트롤러에서 a값 확인해서 넘기기.
+		
+	}else if(actionName.equals("guestDeleteform")) {
+		System.out.println("guestDeleteform 진입");
+		
+		int no = Integer.parseInt(request.getParameter("no"));
+		request.setAttribute("no", no);
+		RequestDispatcher rd = request.getRequestDispatcher("guestDeleteform.jsp"); //"~" : 포워드할 페이지 지정해준 것. 
+		rd.forward(request, response); //url로 확인해보면 포워드 시킨 상태로 guestList.jsp화면이 보여짐.
+	}else if(actionName.equals("delete")) {
+		System.out.println("delete 진입");
+		
+		int no = Integer.parseInt(request.getParameter("no"));
+		String pw = request.getParameter("password");
+		GuestDao dao = new GuestDao();
+		dao.delete(no, pw);
+		response.sendRedirect("gs?a=list");
+	}
+	
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+	}
+
+}
